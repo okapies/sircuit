957c3f69cabdecc633d14aa87eb426d9e4b61374
Yuta Okamoto <okapies@gmail.com>
2015-07-20 02:08:20 +0900

    bump akka to 2.3.12 and add akka-streams and -http

--- project/Build.scala
+++ project/Build.scala
@@ -3,50 +3,36 @@ import Keys._
 
 object SircuitBuild extends Build {
 
-  // sbt-revolver plugin
-  import spray.revolver.RevolverPlugin._
-
   // sbt-assembly plugin
-  import sbtassembly.Plugin._
-  import AssemblyKeys._
+  import sbtassembly.AssemblyPlugin.autoImport._
 
-  // sbt-atmos for Typesafe Console
-  import com.typesafe.sbt.SbtAtmos.{Atmos, atmosSettings, traceAkka}
+  val akkaVersion        = "2.3.12"
+  val akkaStreamsVersion = "1.0"
+  val akkaHttpVersion    = "1.0"
 
-  val akkaVersion = "2.2.4"
-  val sprayVersion = "1.2.1"
-
-  lazy val root = Project(id = "sircuit-server", base = file("."))
-    .dependsOn(uri("https://github.com/okapies/SprayWebSockets.git#2a68215"))
-    .settings(Project.defaultSettings: _*)
-    .settings(Revolver.settings: _*)
-    .settings(assemblySettings: _*)
-    .settings(
+  lazy val sircuitServer = (project in file(".")).
+    settings(
       organization  := "sircuit",
       name          := "sircuit-server",
-      version       := "0.1.0",
-      scalaVersion  := "2.10.4",
+      version       := "0.2.0",
+      scalaVersion  := "2.11.7",
       scalacOptions := Seq("-unchecked", "-deprecation", "-encoding", "utf8"),
       resolvers ++= Seq(
-        "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/",
-        "spray repo" at "http://repo.spray.io/"
+        "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
       ),
       libraryDependencies ++= {
         Seq(
-          "com.typesafe.akka"   %%  "akka-actor"    % akkaVersion,
-          "com.typesafe.akka"   %%  "akka-testkit"  % akkaVersion,
-          "io.spray"            %   "spray-can"     % sprayVersion,
-          "io.spray"            %   "spray-routing" % sprayVersion,
-          "io.spray"            %   "spray-testkit" % sprayVersion,
-          "com.typesafe.slick"  %%  "slick"         % "2.0.1",
-          "com.h2database"      %   "h2"            % "1.4.177",
-          "org.scalatest"       %%  "scalatest"     % "2.1.5" % "test"
+          "com.typesafe.akka"  %% "akka-actor"     % akkaVersion,
+          "com.typesafe.akka"  %% "akka-testkit"   % akkaVersion % "test",
+          "com.typesafe.akka"  %% "akka-stream-experimental"    % akkaStreamsVersion,
+          "com.typesafe.akka"  %% "akka-http-core-experimental" % akkaHttpVersion,
+          "com.typesafe.akka"  %% "akka-http-experimental"      % akkaHttpVersion,
+          "com.typesafe.slick" %% "slick"          % "3.0.0",
+          "com.h2database"     %  "h2"             % "1.4.187",
+          "org.scalatest"      %% "scalatest"      % "2.2.4" % "test"
         )
       },
-      mainClass in assembly := Some("okapies.sircuit.Boot"),
-      traceAkka(akkaVersion)
+      mainClass in assembly := Some("okapies.sircuit.Boot")
     )
-    .configs(Atmos)
-    .settings(atmosSettings: _*)
 
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -2,12 +2,4 @@ resolvers ++= Seq(
   "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
 )
 
-addSbtPlugin("com.typesafe.sbt" % "sbt-atmos-play" % "0.3.2")
-
-addSbtPlugin("io.spray" % "sbt-revolver" % "0.7.1")
-
-addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.10.2")
-
-addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.5.2")
-
-addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.1.5")
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.13.0")
